2454811fbd2a755af8cf5be9369ec0b96ac03252
jjordan08 <jjordan_08@hotmail.com>
2020-10-18 22:14:03 -0500

    v1.1

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-// *********************
-// This Code is for only the floating card in right bottom corner
-// **********************
 
-const touchButton = document.querySelector(".float-text");
-const card = document.querySelector(".float-card-info");
-const close = document.querySelector(".gg-close-r");
-
-touchButton.addEventListener("click", moveCard);
-close.addEventListener("click", moveCard);
-
-function moveCard() {
-    card.classList.toggle("active");
+class PhotoGallery{
+    constructor(){
+        this.API_KEY = '563492ad6f91700001000001e872548da81f42078fca892846b7de21';
+        this.galleryDIv = document.querySelector('.gallery');
+        this.searchForm = document.querySelector('.header form');
+        this.loadMore = document.querySelector('.load-more');
+        this.eventHandle();
+    }
+    eventHandle()
+    {
+        document.addEventListener('DOMContentLoaded', () => {
+            this.getImg();
+        });
+    }
+    async getImg() {
+        const baseURL = 'https://api.pexels.com/v1/curated?per_page=1';
+        const response = await fetch(baseURL, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Autorization: this.API_KEY
+            }
+        });
+        const dat = await response.json();
+        console.log(data)
+    }
 }
-rr => console.log(err))
+
+const gallery = new PhotoGallery();
