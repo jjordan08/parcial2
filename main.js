b7d1a9dd254b7095d4d52ed5222e15e87b090e58
jjordan08 <jjordan_08@hotmail.com>
2020-10-18 23:43:10 -0500

    v1.2

--- main.js
+++ main.js
@@ -12,9 +12,20 @@ class PhotoGallery{
         document.addEventListener('DOMContentLoaded', () => {
             this.getImg();
         });
+        this.searchForm.addEventListener('submit',(e)=>{
+            this.getSearchedImages(e);
+        });
+        this.loadMore.addEventListener('click',(e)=>{
+            this.loadMore()
+        })
     }
     async getImg() {
-        const baseURL = 'https://api.pexels.com/v1/curated?per_page=1';
+        const baseURL = 'https://api.pexels.com/v1/curated?per_page=12';
+        const data = await this.fetchImages(baseURL);
+        this.GenerateHTML(data.photos);
+        console.log(data)
+    }
+    async fetchImages (baseURL){
         const response = await fetch(baseURL, {
             method: 'GET',
             headers: {
@@ -22,8 +33,31 @@ class PhotoGallery{
                 Autorization: this.API_KEY
             }
         });
-        const dat = await response.json();
-        console.log(data)
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    GenerateHTML(photos){
+        photos.forEach(photo=>{
+            const item = document.createElement('div');
+            item.classList.add('item');
+            item.innerHTML = `
+            <a href="#">
+                //lugar donde se encuentran las fotos en el json, ruta
+                <img src="${photo.src.medium}">
+                <h3>${photo.photographer} </h3>
+            </a>`;
+            this.galleryDIv.appendChild(item)
+        })
+    }
+    async getSearchedImages(e){
+        e.preventDefault();
+        this.galleryDIv.innerHTML='';
+        const searchValue = e.target.querySelector('input').value;
+        const baseURL = await `https://api.pexels.com/v1/search?query=${searchValue}nature&per_page=12`;
+        const data = await this.fetchImages(baseURL);
+        this.GenerateHTML(data.photos);
+        e.target.reset();
     }
 }
 
